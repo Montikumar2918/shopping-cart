cc84e902137055ae14092b3771d2a2ae2e5ea2c4
Montikumar2918 <87635622+Montikumar2918@users.noreply.github.com>
2021-09-18 04:10:40 -0400

    Update heading.js

--- src/heading.js
+++ src/heading.js
@@ -5,8 +5,8 @@ function Heading()
           
            <div class="container-fluid container2" id="heading">
     
-           <p class=" shoptitle font-weight-bold">Shopping Cart</p>
-          <p> with the Price </p>
+           <p class=" shoptitle font-weight-bold">Shop in Style</p>
+          <p> with this shop homepage template </p>
           </div>
     );
 }
